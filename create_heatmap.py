d9b7234b24896528349b978eb76080f8e88e311b
adalsteinnpals <adalsteinnpals@gmail.com>
2022-07-19 11:56:37 +0000

    visualiztion

--- create_heatmap.py
+++ create_heatmap.py
@@ -55,20 +55,44 @@ def main(fen_string, include_pieces, method):
 
     mat, chosen_map_keys = get_saliency_mat(board, perturb_pieces, method)
 
-    zeros_mat = np.zeros((8, 8))
+    board_mat = np.zeros((8, 8))
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(20, 10))
 
     for i in range(mat.shape[1]):
-        zeros_mat.ravel()[63 - chosen_map_keys[i]] = mat[0, i]
-    zeros_mat = np.fliplr(zeros_mat)
-    ax2_plot = ax2.imshow(zeros_mat)
+        board_mat.ravel()[63 - chosen_map_keys[i]] = mat[0, i]
+    board_mat = np.fliplr(board_mat)
+
+    max_abs_val = np.abs(board_mat).max()
+    ax2_plot = ax2.imshow(board_mat, vmin=-max_abs_val, vmax=max_abs_val, cmap="bwr")
+
+    ax2.hlines(
+        y=np.arange(0, 8) + 0.5,
+        xmin=np.full(8, 0) - 0.5,
+        xmax=np.full(8, 8) - 0.5,
+        color="black",
+    )
+    ax2.vlines(
+        x=np.arange(0, 8) + 0.5,
+        ymin=np.full(8, 0) - 0.5,
+        ymax=np.full(8, 8) - 0.5,
+        color="black",
+    )
+    ax2.set_xticks(list(range(8)))
+    ax2.set_xticklabels(["A", "B", "C", "D", "E", "F", "G", "H"])
+    ax2.set_yticks(list(range(8)))
+    ax2.set_yticklabels(["8", "7", "6", "5", "4", "3", "2", "1"])
+
+    ax2.set_title(f"Method: {method}, Pieces: {include_pieces}")
+
     plt.colorbar(ax2_plot, ax=ax2)
 
-    for i in range(zeros_mat.shape[0]):
-        for j in range(zeros_mat.shape[1]):
-            c = zeros_mat[j, i]
+    for i in range(board_mat.shape[0]):
+        for j in range(board_mat.shape[1]):
+            c = board_mat[j, i]
             if c != 0:
-                ax2.text(i, j, f"{c:.2f}", va="center", ha="center", c="black")
+                ax2.text(
+                    i, j, f"{c:.2f}", va="center", ha="center", c="white", fontsize=20
+                )
 
     svg = chess.svg.board(board)
     img = cairosvg.svg2png(svg)
